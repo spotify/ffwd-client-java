92d67ad3120778659a8a7330abeb305c5922dacf
Orcun Berkem <o.berkem@gmail.com>
2022-03-29 15:33:48 -0400

    update checkstyle and co

--- ffwd-client/src/main/java/com/spotify/ffwd/v1/Value.java
+++ ffwd-client/src/main/java/com/spotify/ffwd/v1/Value.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,7 +33,8 @@ import java.util.function.Function;
  */
 public abstract class Value {
 
-  Value() {}
+  Value() {
+  }
 
 
   public static Value doubleValue(double value) {
@@ -54,7 +55,8 @@ public abstract class Value {
   @AutoValue
   abstract static class DoubleValue extends Value {
 
-    DoubleValue() {}
+    DoubleValue() {
+    }
 
     @Override
     public final <T> T match(
@@ -76,7 +78,8 @@ public abstract class Value {
   @AutoValue
   abstract static class DistributionValue extends Value {
 
-    DistributionValue() {}
+    DistributionValue() {
+    }
 
     @Override
     public final <T> T match(
